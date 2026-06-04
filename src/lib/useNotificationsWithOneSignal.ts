224394807b01587b5e995ad66c53696db964a88d
Cadehub <ethanbiteckbdbec@gmail.com>
2026-06-04 12:12:57 +0200

    resolution du probleme d'affichage

--- src/lib/useNotificationsWithOneSignal.ts
+++ src/lib/useNotificationsWithOneSignal.ts
@@ -1,9 +1,3 @@
-import { useEffect } from 'react';
-
 export const useNotificationsWithOneSignal = (userId?: string) => {
-  useEffect(() => {
-    console.log("Ancien système de notification OneSignal bypassé.");
-  }, [userId]);
-
   return { unreadCount: 0 };
 };
--- src/utils/firebaseConfig.ts
+++ src/utils/firebaseConfig.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyDTkJkxnbcV3WS-Tm0WHL4ye6IgnvXfrJU',
+  authDomain: 'sentinelle-c4f3b.firebaseapp.com',
+  projectId: 'sentinelle-c4f3b',
+  storageBucket: 'sentinelle-c4f3b.firebasestorage.app',
+  messagingSenderId: '574853926721',
+  appId: '1:574853926721:web:78314a32395e44f1ba9bc0',
 };
 
 export const app = initializeApp(firebaseConfig);
